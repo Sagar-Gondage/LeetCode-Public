e42dd5aade342b728469858cf2115d9493010f88
Sagar-Gondage <sagargondage370@gmail.com>
2022-06-27 19:31:46 +0530

    valid Paranthesis added

---
+++ 20.Valid-Paranthesis.js
@@ -0,0 +1,39 @@
+function method1(s) {
+  let obj = { "(": ")", "[": "]", "{": "}" };
+
+  let stack = [];
+
+  for (let chr of s) {
+    if (obj[chr]) {
+      stack.push(obj[chr]);
+    } else {
+      if (stack.pop() !== chr) return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log(method1("()[]{}"));
+console.log(method1("(]"));
+console.log(method1("({)}"));
+console.log(method1("{[()]}"));
+
+function method2(s) {
+  let stack = [];
+  let obj = { "(": ")", "[": "]", "{": "}" };
+  for (let chr of s) {
+    if (obj[chr]) {
+      stack.push(obj[chr]);
+    } else if (stack.length > 0 && stack[stack.length - 1] === chr) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log(method2("()[]{}"));
+console.log(method2("(]"));
+console.log(method2("({)}"));
+console.log(method2("{[()]}"));
