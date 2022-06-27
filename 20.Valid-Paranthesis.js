cf2d99a545f41c6cb02e9f5646c513fcd8061db3
Sagar-Gondage <sagargondage370@gmail.com>
2022-06-27 19:34:47 +0530

    valid paranthesis updated

--- 20.Valid-Paranthesis.js
+++ 20.Valid-Paranthesis.js
@@ -1,13 +1,13 @@
 function method1(s) {
-  let obj = { "(": ")", "[": "]", "{": "}" };
-
   let stack = [];
-
+  let obj = { "(": ")", "[": "]", "{": "}" };
   for (let chr of s) {
     if (obj[chr]) {
       stack.push(obj[chr]);
+    } else if (stack.length > 0 && stack[stack.length - 1] === chr) {
+      stack.pop();
     } else {
-      if (stack.pop() !== chr) return false;
+      return false;
     }
   }
   return stack.length === 0;
@@ -18,16 +18,17 @@ console.log(method1("(]"));
 console.log(method1("({)}"));
 console.log(method1("{[()]}"));
 
+////  if else part optimised
 function method2(s) {
-  let stack = [];
   let obj = { "(": ")", "[": "]", "{": "}" };
+
+  let stack = [];
+
   for (let chr of s) {
     if (obj[chr]) {
       stack.push(obj[chr]);
-    } else if (stack.length > 0 && stack[stack.length - 1] === chr) {
-      stack.pop();
     } else {
-      return false;
+      if (stack.pop() !== chr) return false;
     }
   }
   return stack.length === 0;
